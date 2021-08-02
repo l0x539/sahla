81a9866746eb466f19d0b015141b92586657db1b
l0x539 <nogues322@gmail.com>
2021-08-02 10:33:00 +0200

    Fix Bug 2

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
-export const API_HOST = "http://api.sahlabusiness.com"
+export const API_HOST = "http://localhost:1337"
 export const LOCAL_API_HOST = "http://localhost:1337"
 export const GOOGLE_PUBLIC_SECRET = "6LeUn9QbAAAAAH6AC-SVu2cWuhiwWYVRdQkjVgRj"
